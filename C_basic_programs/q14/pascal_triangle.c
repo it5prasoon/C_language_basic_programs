93369ca7112888767766274c85d444549e467b09
M@trix <39785096+it5prasoon@users.noreply.github.com>
2019-11-26 13:30:27 +0530

    Update pascal_triangle.c

--- C_basic_programs/q14/pascal_triangle.c
+++ C_basic_programs/q14/pascal_triangle.c
@@ -2,20 +2,20 @@
 
 void main()
 {
-    int row,c=1,k,i,j;
-    printf("Input number of rows: ");
-    scanf("%d",&row);
-    for(i=0;i<row;i++)
+    int rows,var=1,k,i,j;
+    printf("Enter the Number of rows: ");
+    scanf("%d",&rows);
+    for(i=0;i<rows;i++)
     {
-        for(k=1;k<=row-i;k++)
+        for(k=1;k<=rows-i;k++)
         printf("  ");
         for(j=0;j<=i;j++)
         {
             if (j==0||i==0)
-                c=1;
+                var=1;
             else
-               c=c*(i-j+1)/j;
-            printf("%4d",c);
+               var=var*(i-j+1)/j;
+            printf("%4d",var);
         }
         printf("\n");
     }
